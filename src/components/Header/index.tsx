aed81e6500ffc8e2da5b1b4baa0c0b28e819a27a
Giordano94 <giordano.mendes94@gmail.com>
2024-07-02 14:59:07 -0300

    feat(header): create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Header: FC = () => {
+  return (
+    <header className="bg-red-500 p-4">
+      <h1 className="text-white text-2xl">Pokedex</h1>
+    </header>
+  );
+};
+
+export default Header;
