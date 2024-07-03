f41c6ba8a760e29c4bf8a29f5060fbb390f7ec9d
Giordano94 <giordano.mendes94@gmail.com>
2024-07-03 15:02:40 -0300

    feat(header): style Header and add pokemon logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
+import pokemonLogo from '/public/images/pokemon-logo.png';
 
 const Header: FC = () => {
   return (
-    <header className="bg-red-500 p-4">
-      <h1 className="text-white text-2xl">Pokedex</h1>
+    <header className="bg-purple-600 flex items-center justify-center h-28 mb-3">
+      <img className="w-64" src={pokemonLogo} alt="pokemon-logo" />
     </header>
   );
 };
