3d642ce05485bff78d0125dea25a5aa624ee866d
Giordano94 <giordano.mendes94@gmail.com>
2024-07-03 20:06:01 -0300

    refactor: update fetchPokemons function to handle search term

--- src/data/service.tsx
+++ src/data/service.tsx
@@ -3,8 +3,17 @@ import { Pokemon, PokemonDetails } from '../ types';
 
 const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
-export const fetchPokemons = async (): Promise<PokemonDetails[]> => {
+export const fetchPokemons = async (
+  searchTerm?: string
+): Promise<PokemonDetails[]> => {
   try {
+    if (searchTerm) {
+      const response = await axios.get<PokemonDetails>(
+        `${BASE_URL}/${searchTerm.toLowerCase()}`
+      );
+      return [response.data];
+    }
+
     const response = await axios.get(`${BASE_URL}?limit=151`);
     const pokemons: Pokemon[] = response.data.results;
 
