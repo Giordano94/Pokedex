f2019b002598b9281559b212d0b77918b0ca8f0c
Giordano94 <giordano.mendes94@gmail.com>
2024-07-03 10:40:52 -0300

    feat(service):add fetchPokemons function to retrieve detailed Pokemon data

---
+++ src/data/service.tsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { Pokemon, PokemonDetails } from '../ types';
+
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+export const fetchPokemons = async (): Promise<PokemonDetails[]> => {
+  try {
+    const response = await axios.get(`${BASE_URL}?limit=150`);
+    const pokemons: Pokemon[] = response.data.results;
+
+    const detailedPokemons = await Promise.all(
+      pokemons.map(async (pokemon) => {
+        const pokemonDetail = await axios.get<PokemonDetails>(pokemon.url);
+        return pokemonDetail.data;
+      })
+    );
+
+    return detailedPokemons;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
+};
