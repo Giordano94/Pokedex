43fd364453f4ff434d6bae5bb61cc59784de6e6b
Giordano94 <giordano.mendes94@gmail.com>
2024-07-03 11:54:07 -0300

    feat(utils): create formatPokemonId function

---
+++ src/utils/format-pokemon-id.ts
@@ -0,0 +1,9 @@
+export const formatPokemonId = (id: number): string => {
+  if (id < 10) {
+    return `#000${id}`;
+  } else if (id < 100) {
+    return `#00${id}`;
+  } else {
+    return `#${id}`;
+  }
+};
