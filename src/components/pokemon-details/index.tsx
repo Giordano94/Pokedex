0ddd8a727eb92e769d359dd64ae49b73f22bcc73
Giordano94 <giordano.mendes94@gmail.com>
2024-07-04 14:21:13 -0300

    feat(details): Add fetchPokemonDetailsData function to Details  page

--- src/components/pokemon-details/index.tsx
+++ src/components/pokemon-details/index.tsx
@@ -1,11 +1,18 @@
 import { FC } from 'react';
+import { formatPokemonId } from '../../utils/format-pokemon-id';
+import { PokemonDetails as PokemonDetailsType } from '../../ types';
 
-interface PokemonDetailsProps {}
+interface PokemonDetailsProps {
+  details: PokemonDetailsType;
+}
 
-const PokemonDetails: FC<PokemonDetailsProps> = () => {
+const PokemonDetails: FC<PokemonDetailsProps> = ({ details }) => {
+  const { name, id, sprites, abilities } = details;
   return (
     <div>
-      <h1>Details</h1>
+      <h1>{`${name} Nº${formatPokemonId(id)} `}</h1>
+      <img src={sprites.front_default} alt={name} />
+      <h2>{abilities[1].effect}</h2>
     </div>
   );
 };
