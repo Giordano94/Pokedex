80996d32e863128a09aa3ad14fb53c7d3ee043dd
Giordano94 <giordano.mendes94@gmail.com>
2024-07-04 16:54:59 -0300

    feat(pokemon-details): add infoCard to the PokemonDetails component

--- src/components/pokemon-details/index.tsx
+++ src/components/pokemon-details/index.tsx
@@ -1,18 +1,25 @@
 import { FC } from 'react';
 import { formatPokemonId } from '../../utils/format-pokemon-id';
 import { PokemonDetails as PokemonDetailsType } from '../../ types';
+import InfoCard from '../info-card';
 
 interface PokemonDetailsProps {
   details: PokemonDetailsType;
 }
 
 const PokemonDetails: FC<PokemonDetailsProps> = ({ details }) => {
-  const { name, id, sprites } = details;
+  const { name, id, sprites, height, weight, abilities, types } = details;
 
   return (
     <div>
       <h1>{`${name} Nº${formatPokemonId(id)} `}</h1>
       <img src={sprites.front_default} alt={name} />
+      <InfoCard
+        height={height}
+        weight={weight}
+        abilities={abilities}
+        types={types.map((type) => type.type.name)}
+      />
     </div>
   );
 };
