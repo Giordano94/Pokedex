aa3489af3d31cad1862102dfc295c554b46def37
Giordano94 <giordano.mendes94@gmail.com>
2024-07-04 14:09:50 -0300

    feat(types): add ability to the PokemonDetails interface

--- src/ types.ts
+++ src/ types.ts
@@ -3,6 +3,10 @@ export interface Pokemon {
   url: string;
 }
 
+export interface Ability {
+  effect: string;
+}
+
 export interface PokemonDetails {
   id: number;
   name: string;
@@ -10,4 +14,5 @@ export interface PokemonDetails {
   weight: number;
   types: Array<{ type: { name: string } }>;
   sprites: { front_default: string };
+  abilities: Ability[];
 }
