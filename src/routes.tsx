a627a49040647a38562b38eff1e719991745e761
Giordano94 <giordano.mendes94@gmail.com>
2024-07-04 10:19:31 -0300

    feat(routes): add route for details page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/home-page';
+import DetailsPage from './pages/details';
 
 export const routes = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/pokemon/:id',
+    element: <DetailsPage />,
   },
 ]);
