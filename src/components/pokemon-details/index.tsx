116dd85c2ee1fed699bfbcc28dabdaba723d9960
Giordano94 <giordano.mendes94@gmail.com>
2024-07-04 10:10:54 -0300

    feat(pokemon-details): add PokemonDetails component

---
+++ src/components/pokemon-details/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+interface PokemonDetailsProps {}
+
+const PokemonDetails: FC<PokemonDetailsProps> = () => {
+  return (
+    <div>
+      <h1>Details</h1>
+    </div>
+  );
+};
+
+export default PokemonDetails;
