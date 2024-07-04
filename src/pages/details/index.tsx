0505be8af57d3a8ac16512f07f919b2bb0b00b2e
Giordano94 <giordano.mendes94@gmail.com>
2024-07-04 14:17:50 -0300

    feat(details): Add fetchPokemonDetailsData function to Details  page

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,9 +1,33 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { PokemonDetails as PokemonDetailsType } from '../../ types';
 import PokemonDetails from '../../components/pokemon-details';
+import { fetchPokemonDetails } from '../../data/service';
 
 interface DetailsPageProps {}
 
 const DetailsPage: FC<DetailsPageProps> = () => {
+  const [pokemonDetails, setPokemonDetails] = useState<PokemonDetailsType>();
+
+  const { id } = useParams();
+
+  const fetchPokemonDetailsData = useCallback(async () => {
+    try {
+      if (id) {
+        const data = await fetchPokemonDetails(id);
+        if (data) {
+          setPokemonDetails(data);
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    fetchPokemonDetailsData();
+  }, [fetchPokemonDetailsData]);
+
   return (
     <div>
       <PokemonDetails />
