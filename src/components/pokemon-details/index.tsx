1735d8debe7d9176a866de20c394a3e2237d32dc
Giordano94 <giordano.mendes94@gmail.com>
2024-07-07 12:33:28 -0300

    feat(pokemon-details): add Header component and structure style inside component

--- src/components/pokemon-details/index.tsx
+++ src/components/pokemon-details/index.tsx
@@ -3,6 +3,7 @@ import { formatPokemonId } from '../../utils/format-pokemon-id';
 import { PokemonDetails as PokemonDetailsType } from '../../ types';
 import InfoCard from '../info-card';
 import CharacteristicsCard from '../characteristics-card';
+import Header from '../header';
 
 interface PokemonDetailsProps {
   details: PokemonDetailsType;
@@ -21,25 +22,30 @@ const PokemonDetails: FC<PokemonDetailsProps> = ({ details }) => {
   } = details;
 
   return (
-    <div className="text-center">
-      <h1 className="text-5xl font-bold mb-4">{`${
-        name.charAt(0).toUpperCase() + name.slice(1)
-      } |  Nº${formatPokemonId(id)}`}</h1>
-      <img
-        src={sprites.other.showdown.front_default}
-        alt={name}
-        className="mx-auto mb-4 h-80 w-80"
-      />
-      <div className="flex justify-center items-center gap-4">
-        <CharacteristicsCard
-          characteristics={characteristics.descriptions[7].description}
-        />
-        <InfoCard
-          height={height}
-          weight={weight}
-          abilities={abilities}
-          types={types.map((type) => type.type.name)}
-        />
+    <div>
+      <Header />
+      <div className="flex justify-center items-center">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold mb-4">{`${
+            name.charAt(0).toUpperCase() + name.slice(1)
+          } |  Nº${formatPokemonId(id)}`}</h1>
+          <img
+            src={sprites.other.showdown.front_default}
+            alt={name}
+            className="mx-auto mb-4 h-80 w-80"
+          />
+          <div className="flex justify-center items-center gap-4">
+            <CharacteristicsCard
+              characteristics={characteristics.descriptions[7].description}
+            />
+            <InfoCard
+              height={height}
+              weight={weight}
+              abilities={abilities}
+              types={types.map((type) => type.type.name)}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
