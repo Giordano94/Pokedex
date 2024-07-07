a4abdd9619be2cbdcd7f0b9ed477d207fbf8c33f
Giordano94 <giordano.mendes94@gmail.com>
2024-07-07 19:04:07 -0300

    feat(header): adjust Header component to handle route-specific logo visibility and layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,34 @@
 import { FC } from 'react';
 import pokemonLogo from '/public/images/pokemon-logo.png';
 import SearchInput from '../search-input';
+import { useLocation } from 'react-router-dom';
 
 interface HeaderProps {
   onSearchChange?: (value: string) => void;
 }
 
 const Header: FC<HeaderProps> = ({ onSearchChange }) => {
+  const location = useLocation();
+  const isPokemonRoute = location.pathname.includes('/pokemon/');
+
   return (
-    <header className="bg-purple-600 flex justify-between items-center h-28 fixed left-0 right-0 z-50">
+    <header
+      className={`bg-purple-600 flex items-center h-28 ${
+        isPokemonRoute
+          ? 'justify-center'
+          : ' justify-between fixed left-0 right-0 z-50'
+      }`}
+    >
       <img
-        className="w-64 hidden md:block"
+        className={`w-64 ${isPokemonRoute ? '' : 'hidden md:block'} `}
         src={pokemonLogo}
         alt="pokemon-logo"
       />
-      <div className="mt-6 flex-grow md:flex-grow-0">
-        <SearchInput onChange={onSearchChange ? onSearchChange : () => {}} />
-      </div>
+      {!isPokemonRoute && (
+        <div className="mt-6 flex-grow md:flex-grow-0">
+          <SearchInput onChange={onSearchChange ? onSearchChange : () => {}} />
+        </div>
+      )}
     </header>
   );
 };
