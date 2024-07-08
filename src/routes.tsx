f46220300139d53283f5bfae71b235471b08765b
Giordano94 <giordano.mendes94@gmail.com>
2024-07-08 14:11:30 -0300

    feat(routes): add Root component and refactor routes to nested structure

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/home-page';
 import DetailsPage from './pages/details';
+import Root from './pages/Root';
 
 export const routes = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/pokemon/:id',
-    element: <DetailsPage />,
+    element: <Root />,
+    children: [
+      {
+        path: 'pokemons',
+        element: <Home />,
+      },
+      {
+        path: 'pokemon-details/:id',
+        element: <DetailsPage />,
+      },
+    ],
   },
 ]);
